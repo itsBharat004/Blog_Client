c0d04ff42ffcef59368df3b836ca248a7896d592
yash gupta <yashgupta1mole@gmail.com>
2023-09-20 00:09:53 +0530

    final

--- src/Components/GenericComonents/FallBack.jsx
+++ src/Components/GenericComonents/FallBack.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Top from '../Header/Top'
 
 const FallBack = () => {
   return (
-    <div>
-      <h1>you are not login yet</h1> 
-      <h2>  create your account <Link to="/Register">signUp</Link> </h2>
-      <h2>  Or login <Link to="/Login">Login</Link> </h2>
+    <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
+      <Top/>
+      <h3>you are not login yet</h3> 
+      <p>  create your account <Link to="/Register"style={{color:"blue",textDecoration:"underLine"}}>signUp</Link> </p>
+      <p>  Or login <Link to="/Login" style={{color:"blue",textDecoration:"underLine"}}>Login</Link> </p>
     </div>
   )
 }

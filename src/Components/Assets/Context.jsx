69cafa9194bb289849ab13e2e0cae750fe04a59b
yash gupta <yashgupta1mole@gmail.com>
2023-09-22 15:01:23 +0530

    add loader

--- src/Components/Assets/Context.jsx
+++ src/Components/Assets/Context.jsx
@@ -1,10 +1,11 @@
-import React, { useEffect } from "react";
-import { createContext, useState } from "react";
+import { useEffect, createContext, useState } from "react";
 import axios from "axios";
+import BeatLoader from "react-spinners/ClipLoader";
 const dataa = createContext();
 
 const Context = ({ children }) => {
   const [arrObj, setArrObj] = useState("");
+  const [loading,setLoading]=useState(true);
 
   useEffect(() => {
     const baseUrl = `${import.meta.env.VITE_ULR}/Artical`;
@@ -12,9 +13,34 @@ const Context = ({ children }) => {
     // const baseUrl="http://localhost:4040/Artical";
     axios.get(baseUrl).then((info) => setArrObj(info.data));
     console.log(arrObj);
+    setLoading(!loading);
+   
   }, []);
+//   function load(){
+//     setLoading(!loading);
+// console.log(loading);
+//   }
+  
   return (
-    <>{arrObj && <dataa.Provider value={arrObj}>{children}</dataa.Provider>}</>
+    <>
+    {/* <button onClick={load}>click</button> */}
+    {arrObj &&
+    <dataa.Provider value={arrObj}>
+      {children}
+      </dataa.Provider> 
+     
+    }
+  { loading&&
+   <div style={{position:"absolute", left:"50%",top:"50%",transform:"translate(-50%,-50%)"}}>
+
+     <BeatLoader
+      color="#0001ff"
+      loading={loading}
+      speedMultiplier={1}
+      />
+      <p>the siren blog website</p>
+    </div>}
+    </>
   );
 };
 
